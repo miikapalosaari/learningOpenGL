41ad7968961ba8d830c041c93e0dc615a1822722
miikapalosaari <miika.palosaari@gmail.com>
2025-02-15 16:52:40 +0200

    10 oddly rotated cubes

--- src/main_shader_class_v2.cpp
+++ src/main_shader_class_v2.cpp
@@ -247,7 +247,16 @@ int main()
 		// render container
 		glBindVertexArray(VAO);
 		
-		glDrawArrays(GL_TRIANGLES, 0, 36);
+		for (unsigned int i = 0; i < 10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			simpleShader.setMat4("model", model);
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+		
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
 		// -------------------------------------------------------------------------------
